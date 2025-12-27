1d57cdcd742bbec1ae8cc8551b32d56bce0e975b
phihes <phihes@gmail.com>
2025-12-27 13:30:41 +0100

    add deploy to github pages action

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: 'https://phihes.github.io',
+  base: '/pruefportal-landing-page',
   integrations: [mdx(), sitemap(), react()],
   output: "static",
 
